878bd33ef32cd549554a869e3c0542ab6ee3a35a
Sandhya Kumari <111334390+sandhyakri19@users.noreply.github.com>
2022-09-01 13:34:50 +0530

    Add files via upload

---
+++ String_methods.py
@@ -0,0 +1,37 @@
+""" 1st Sept 2022 Thursday 
+Topic: String Methods """
+# print(dir(str)) # To know display all string methods
+
+# a=input()
+# print(a.upper())
+# print(a.lower())
+# print(a.swapcase())
+# print(a.isupper())
+# print(a.islower())
+# print(a.capitalize())
+# print(a.iscapitalize()) # AttributeError
+# print(a.title())
+# print(a.istitle())
+
+# print('_ab'.isidentifier())
+
+# a='python'
+# print(len(a))
+# print(len('programming'))
+
+# print(''.isspace())
+# print('  p '.isspace())
+# print(' '.isspace())
+# print('           '.isspace())
+
+# print('34'.isalnum())
+# print('hi5'.isalnum())
+# print('hi_5'.isalnum())
+
+# print('b5'.isalpha())
+# print('cricket'.isalpha())
+
+# print('python'.count('p'))
+# print('python'.count('py'))
+# print('python'.count('ph'))
+# print('python'.count('P'))
